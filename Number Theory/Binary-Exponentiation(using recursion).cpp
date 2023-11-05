dc2c6c5cf8b9782db016f02dea039ae72d4445bf
Wakil Ahammad <69745195+Wakil-Ahammad@users.noreply.github.com>
2023-11-05 19:28:17 +0600

    Update Binary-Exponentiation(using recursion).cpp

--- Number Theory/Binary-Exponentiation(using recursion).cpp
+++ Number Theory/Binary-Exponentiation(using recursion).cpp
@@ -1,17 +1,9 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int M = 1e9+7;
 
+const int M = 1e9+7;
 int BinExp(int a,int b){
     if(b==0)return 1;
     long long r = BinExp(a,b/2);
     if(b%2)return (a*((r*r)%M))%M;
     else return (r*r)%M;
     return r;
-}
-
-int main(){
-    int a,b;
-    cin>>a>>b;
-    cout<<BinExp(a,b)<<endl;
 }

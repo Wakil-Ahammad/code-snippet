2aa00996646cb1230767cfee56570e38c5a77d70
Wakil Ahammad <69745195+Wakil-Ahammad@users.noreply.github.com>
2023-11-08 23:17:14 +0600

    Update Prime Factor single elment.cpp

--- Number Theory/Prime Factor single elment.cpp
+++ Number Theory/Prime Factor single elment.cpp
@@ -1,1 +1,16 @@
-
+void PrimeFact(int n){
+map<int,int>mp;
+for(int i=2;i*i<=n;i++){
+    if(n%i==0){
+       while(n%i==0) {
+          mp[i]++;
+          n=n/i;
+       }
+    }
+}
+  if(n>1) mp[n]++;
+  
+  for(auto it:mp){
+     cout<<it.first<<" "<<it.second<<endl;
+  }
+}

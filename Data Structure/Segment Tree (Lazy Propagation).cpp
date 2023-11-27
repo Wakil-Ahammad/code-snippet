164a8babd29eff115f0dc797df2554146683f37c
Wakil Ahammad <69745195+Wakil-Ahammad@users.noreply.github.com>
2023-11-27 19:12:11 +0600

    Update Segment Tree (Lazy Propagation).cpp

--- Data Structure/Segment Tree (Lazy Propagation).cpp
+++ Data Structure/Segment Tree (Lazy Propagation).cpp
@@ -1,52 +1,66 @@
-const int N = 1e5+9;
+const ll N = 1e5+5;
+ll t[N*4],lazy[N*4];
 ll a[N];
-struct node {
-    ll prop,val;
-};
-node t[N*4];
 
-void build(int node,int b, int e) {
-   if(b==e) {
-      t[node].val = a[b];
-      //t[node].prop = 0;
-      return;
-   }
-   int l = 2*node;
-   int r = 2*node+1;
-   int mid = (b+e)/2;
-   build(l,b,mid);
-   build(r,mid+1,e);
-   
-   t[node].val = t[l].val + t[r].val;
-   //t[node].prop = 0;
-}
-
-
-void update(ll node,ll b,ll e,ll i,ll j,ll x){
-    if(b>j or e<i) return;
-    if(b>=i and e<=j){
-        t[node].val +=((e-b+1)*x);
-        t[node].prop += x;
+void push(ll node,ll b,ll e){
+    if(lazy[node]==-1){
         return;
     }
+
+    t[node] = (e-b+1)*lazy[node];
+    
+    if(b!=e) {
+      ll mid = (b+e)/2;
+      ll l = 2*node, r = 2*node+1;
+      lazy[l] = lazy[node];
+      lazy[r] = lazy[node];
+   }
+
+   lazy[node] = -1;
+}
+
+void build(ll node,ll b,ll e){
+
+    lazy[node] = -1;
+    if(b==e){
+        t[node] = a[b];
+        return;
+    }
+
+    ll mid = (b+e)/2;
+    ll l = 2*node,r = 2*node+1;
+    build(l,b,mid);
+    build(r,mid+1,e);
+
+    t[node] = t[l]+t[r];
+}
+
+void update(ll node,ll b,ll e,ll i,ll j,ll x){
+    push(node,b,e);
+    if(b>j or e<i) return;
+    if(b>=i and e<=j){
+       lazy[node] = x;
+       push(node,b,e);
+       return; 
+    }
+
     ll mid = (b+e)/2;
     ll l = 2*node,r = 2*node+1;
     update(l,b,mid,i,j,x);
     update(r,mid+1,e,i,j,x);
-    t[node].val = t[l].val + t[r].val + (e-b+1)*t[node].prop;
+
+    t[node] = t[l]+t[r];
+
 }
 
-ll query(ll node,ll b,ll e,ll i,ll j,ll carry){
-     if(b>j or e<i) return 0;
-     if(b>=i and e<=j){
-        return t[node].val+((e-b+1)*carry);
-     }
+ll query(ll node,ll b,ll e,ll i,ll j){
+    push(node,b,e);
+    if(b>j or e<i) return 0;
+    if(b>=i and e<=j){
+        return t[node];
+    }
 
-     ll mid = (b+e)/2;
-     ll l = 2*node;
-     ll r = 2*node+1;
-
-     ll t1 = query(l,b,mid,i,j,carry+t[node].prop);
-     ll t2 = query(r,mid+1,e,i,j,carry+t[node].prop);
-     return t1 + t2;
+    ll mid = (b+e)/2;
+    ll l = 2*node,r = 2*node+1;
+    return query(l,b,mid,i,j) + query(r,mid+1,e,i,j);
 }

e29cee35ce58ba971628ddca4fe6f37fc73b0110
Wakil Ahammad <69745195+Wakil-Ahammad@users.noreply.github.com>
2023-12-07 16:42:33 +0600

    Create Dice Combination.cpp

---
+++ Dynamic Programmin/Dice Combination.cpp
@@ -0,0 +1,16 @@
+const ll N = 1e6+5;
+ll dp[N];
+ll n;
+ll f(ll x){
+    if(x==n) return 1;
+    if(x>n) return 0;
+
+    if(dp[x]!=-1) return dp[x];
+
+    ll res = 0;
+    for(ll i=1;i<=6;i++){
+        res=( res+f(x+i))%mod;
+    }
+
+    return dp[x] = res;
+}
